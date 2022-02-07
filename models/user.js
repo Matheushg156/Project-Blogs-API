94e58d83711e1ff265af50926997de1db9587edb
Matheus Henrique Gonzaga <matheushg@pop-os.localdomain>
2022-02-07 18:59:01 -0300

    Refc models/User

--- models/user.js
+++ models/user.js
@@ -5,21 +5,16 @@ const {
 } = require('../helpers/validateFields');
 
 module.exports = (sequelize, DataTypes) => {
-  const Users = sequelize.define('Users', {
+  const User = sequelize.define('Users', {
     id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
     displayName: { type: DataTypes.STRING, allowNull: false, validate: displayNameValidator,
     },
-    email: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: emailValidator,
-    },
-    password: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: passwordValidator,
-    },
+    email: { type: DataTypes.STRING, allowNull: false, validate: emailValidator },
+    password: { type: DataTypes.STRING, allowNull: false, validate: passwordValidator },
     image: DataTypes.STRING,
+  },
+  {
+    tableName: 'Users', timestamps: false,
   });
-  return Users;
+  return User;
 };

cbcc8beb60f0f66350e8627362fcf234d0b2e9ac
Matheus Henrique Gonzaga <matheushg@pop-os.localdomain>
2022-02-08 23:16:26 -0300

    refc createUser

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,13 +3,13 @@ const UserServices = require('../services/userService');
 const createUser = async (req, res) => {
   const { displayName, email, password, image } = req.body;
   const user = await UserServices.validateUser({ displayName, email, password, image });
-  if (user.errors[0].message === 'email must be unique') {
-    return res.status(409).json({ message: 'User already registered' });
+  if (!user.errors) {
+    res.status(201).json(user);
+  } else if (user.errors[0].message === 'email must be unique') {
+    res.status(409).json({ message: 'User already registered' });
+  } else {
+    res.status(400).json({ message: user.errors[0].message });
   }
-  if (user.errors) {
-    return res.status(400).json({ message: user.errors[0].message });
-  }
-  res.status(201).json(user);
 };
 
 module.exports = {
